84793d004acc9013a8959e1acaf1d926d86a6c9a
Wojtek <layvin@interia.pl>
2023-06-05 13:23:46 +0200

    Format kodu

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+
 # Printowanie macierzy z liczbami na lewo
 
 def lewo(wiersze):
@@ -9,25 +11,25 @@ def lewo(wiersze):
         zmienione_wiersze.append(zmieniony_wiersz)
     return '\n'.join(zmienione_wiersze)
 
-#wiersze = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
-#wyjscie = lewo(wiersze)
-#print(wyjscie)
+
+# wiersze = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
+# wyjscie = lewo(wiersze)
+# print(wyjscie)
 
 # Printowanie macierzy z liczbami na prawo
 
 def porzadkowanie_na_prawo(wiersze):
     macierz = np.matrix(wiersze)
     return str(macierz)
+
+
 print(porzadkowanie_na_prawo([[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]))
 
+# Test prawo
+
 input = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
-
-
 oczekiwany_output = '[[   1    2   10  150]\n [  10    2 1000    2]\n [   1  120    1 1000]]'
-
 assert porzadkowanie_na_prawo(input) == oczekiwany_output
-
-#print(porzadkowanie_na_prawo([[1, 2, 10, 150],[10, 2, 1000, 2],[1, 120, 1, 1000]]))
 
 # Test lewo
 
