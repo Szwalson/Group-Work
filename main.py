3d0f6b538ab51afd234b092b198403a971a5f5d0
Ligia Kaczmarek <75280@ue.poznan.pl>
2023-06-05 12:17:16 +0200

    Print_na_lewo

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#Jedziemy
+def lewo(wiersze):
+    szerokosc = [max(len(str(wiersz[i])) for wiersz in wiersze) for i in range(len(wiersze[0]))]
+    zmienione_wiersze = []
+    for wiersz in wiersze:
+        zmieniony_wiersz = '[{}]'.format('  ,  '.join(str(val).ljust(width) for val, width in zip(wiersz, szerokosc)))
+        zmienione_wiersze.append(zmieniony_wiersz)
+    return '\n'.join(zmienione_wiersze)
+wiersze = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
+wyjscie = lewo(wiersze)
+print(wyjscie)
