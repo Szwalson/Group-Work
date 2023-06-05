6e03b6da2f32e02b9b791acca060e9c2d9931097
Wojtek <layvin@interia.pl>
2023-06-05 13:20:15 +0200

    Test lewo

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+# Printowanie macierzy z liczbami na lewo
+
 def lewo(wiersze):
     szerokosc = [max(len(str(wiersz[i])) for wiersz in wiersze) for i in range(len(wiersze[0]))]
     zmienione_wiersze = []
@@ -5,14 +8,21 @@ def lewo(wiersze):
         zmieniony_wiersz = '[{}]'.format('  ,  '.join(str(val).ljust(width) for val, width in zip(wiersz, szerokosc)))
         zmienione_wiersze.append(zmieniony_wiersz)
     return '\n'.join(zmienione_wiersze)
-wiersze = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
-wyjscie = lewo(wiersze)
-print(wyjscie)
+
+#wiersze = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
+#wyjscie = lewo(wiersze)
+#print(wyjscie)
 
 # Printowanie macierzy z liczbami na prawo
 
 def porzadkowanie_na_prawo(wiersze):
-    import numpy as np
     macierz = np.matrix(wiersze)
     return macierz
 
+#print(porzadkowanie_na_prawo([[1, 2, 10, 150],[10, 2, 1000, 2],[1, 120, 1, 1000]]))
+
+# Test lewo
+
+input1 = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
+output1 = '[1   ,  2    ,  10    ,  150 ]\n[10  ,  2    ,  1000  ,  2   ]\n[1   ,  120  ,  1     ,  1000]'
+assert lewo(input1) == output1
