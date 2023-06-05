98901a0c7bc0238f229afff81fd3bc6485688a6d
Ligia Kaczmarek <75280@ue.poznan.pl>
2023-06-05 12:34:47 +0200

    Add lewo

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def lewo(wiersze):
     szerokosc = [max(len(str(wiersz[i])) for wiersz in wiersze) for i in range(len(wiersze[0]))]
     zmienione_wiersze = []
@@ -9,11 +8,11 @@ def lewo(wiersze):
 wiersze = [[1, 2, 10, 150], [10, 2, 1000, 2], [1, 120, 1, 1000]]
 wyjscie = lewo(wiersze)
 print(wyjscie)
-=======
+
 # Printowanie macierzy z liczbami na prawo
 
 def porzadkowanie_na_prawo(wiersze):
     import numpy as np
     macierz = np.matrix(wiersze)
     return macierz
->>>>>>> 06c03297c5d0ade50ee793907a816a516d17e77c
+
